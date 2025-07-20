eae626bf59bb58ad4e77121a378e418c323e595f
Mduduzi Sibisi <108700682+OrigamiFolds@users.noreply.github.com>
2025-07-20 11:08:17 +0200

    Add files via upload

---
+++ src/main/java/draft/dev/kycprovedemo/ProveIdentityDemoApplication.java
@@ -0,0 +1,13 @@
+package draft.dev.kycprovedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveIdentityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveIdentityDemoApplication.class, args);
+	}
+
+}
